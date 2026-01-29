d02c6754976c063bd4f03cdd7278baef9d7d8a88
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2026-01-29 15:41:02 -0300

    chore: config worker start

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,11 +1,15 @@
 import { Account, Client, Databases, TablesDB } from "appwrite";
+
 const APPWRITE_ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
+const APPWRITE_PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+export const APPWRITE_DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+export const APPWRITE_TABLE_ID = import.meta.env.VITE_APPWRITE_TABLE_ID;
 
 const client = new Client();
 
 client
   .setEndpoint(APPWRITE_ENDPOINT)
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  .setProject(APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
