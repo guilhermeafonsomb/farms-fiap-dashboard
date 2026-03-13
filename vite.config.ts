fc13a00aa44b7075f3a4998dcd4e825a1ff22e02
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2026-03-13 09:57:18 -0300

    feat: migrate to zephyr

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import federation from "@originjs/vite-plugin-federation";
+import { withZephyr } from "vite-plugin-zephyr";
 import path from "path";
 
 export default defineConfig({
   plugins: [
     react(),
-    federation({
-      name: "dashboard-app",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./FarmsFiapDashboard": "./src/App.tsx",
+    withZephyr({
+      mfConfig: {
+        name: "dashboard-app",
+        filename: "remoteEntry.js",
+        exposes: {
+          "./FarmsFiapDashboard": "./src/App.tsx",
+        },
+        shared: {
+          react: { singleton: true, requiredVersion: "^19.0.0" },
+          "react-dom": { singleton: true, requiredVersion: "^19.0.0" },
+          "react-router-dom": { singleton: true },
+          "@tanstack/react-query": { singleton: true },
+          zustand: { singleton: true },
+          clsx: {},
+        },
+        dts: false,
       },
-      shared: [
-        "react",
-        "react-dom",
-        "tailwindcss",
-        "postcss",
-        "autoprefixer",
-        "react-router-dom",
-        "@tanstack/react-query",
-        "clsx",
-        "zustand",
-      ],
     }),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  optimizeDeps: {
+    needsInterop: ["react", "@tanstack/react-query", "clsx", "zustand"],
   },
   build: {
     target: "esnext",
